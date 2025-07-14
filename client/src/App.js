d8e9e568df127d300525e72af223e7aac40d9f76
rahulwagh09 <rahulwagh28032003@gmail.com>
2025-07-14 14:49:33 +0000

    updated the version

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import OtherPage from './OtherPage';
-import Fib from './Fib';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import OtherPage from "./OtherPage";
+import Fib from "./Fib";
+ 
 function App() {
   return (
     <Router>
@@ -22,13 +22,13 @@ function App() {
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
-        <div>
-          <Route exact path="/" component={Fib} />
-          <Route path="/otherpage" component={OtherPage} />
-        </div>
+        <Routes>
+          <Route path="/" element={<Fib />} />
+          <Route path="/otherpage" element={<OtherPage />} />
+        </Routes>
       </div>
     </Router>
   );
 }
-
+ 
 export default App;
